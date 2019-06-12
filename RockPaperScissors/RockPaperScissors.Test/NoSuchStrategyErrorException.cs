c9dc95b2bfad0264788d328226eb0a87e333a357
abeppler <abeppler2000@gmail.com>
2019-06-12 17:55:04 -0300

    RpsGameWinner_ShouldRaiseNoSuchStrategyErrorWhenStrategyDifferentRPS
    RpsGameWinner_PlayerOneShouldWinWhenBothPlayersPlaySameMove

---
+++ RockPaperScissors/RockPaperScissors.Test/NoSuchStrategyErrorException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RockPaperScissors.Test
+{
+    public class NoSuchStrategyErrorException : Exception
+    {
+        public NoSuchStrategyErrorException()
+        {
+        }
+
+        public NoSuchStrategyErrorException(string message) : base(message)
+        {
+        }
+
+        public NoSuchStrategyErrorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NoSuchStrategyErrorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- RockPaperScissors/RockPaperScissors.Test/UnitTest1.cs
+++ RockPaperScissors/RockPaperScissors.Test/UnitTest1.cs
@@ -1,5 +1,7 @@
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace RockPaperScissors.Test
@@ -9,6 +11,7 @@ namespace RockPaperScissors.Test
         [Fact]
         public void RpsGameWinner_ShouldRaiseExceptionWhenPlayerNumbersNotEqualTwo()
         {
+            //Arrange
             var round1 = new Round
             {
                 Game1 = new Game()
@@ -29,14 +32,75 @@ namespace RockPaperScissors.Test
                 }
             };
 
+            //Act & Assert
             var ex2 = Assert.Throws<WrongNumberOfPlayersErrorException>(() => RpsGameWinner(round2));
         }
 
-        private object RpsGameWinner(Round round)
+        [Fact]
+        public void RpsGameWinner_ShouldRaiseNoSuchStrategyErrorWhenStrategyDifferentRPS()
+        {
+            //Arrange
+            var round = new Round
+            {
+                Game1 = new Game()
+                {
+                    PlayerName = "Dave",
+                    Strategy = "T"
+                },
+                Game2 = new Game()
+                {
+                    PlayerName = "Dave",
+                    Strategy = "V"
+                }
+            };
+
+            //Act & Assert
+            var ex = Assert.Throws<NoSuchStrategyErrorException>(() => RpsGameWinner(round));
+        }
+
+
+        [Fact]
+        public void RpsGameWinner_PlayerOneShouldWinWhenBothPlayersPlaySameMove()
+        {
+            //Arrange
+            var round = new Round
+            {
+                Game1 = new Game()
+                {
+                    PlayerName = "Player1",
+                    Strategy = "R"
+                },
+                Game2 = new Game()
+                {
+                    PlayerName = "Player2",
+                    Strategy = "R"
+                }
+            };
+
+            //Act
+            var winner = RpsGameWinner(round);
+
+            //Assert
+            winner.PlayerName.Should().Be("Player1");
+        }
+
+        private Game RpsGameWinner(Round round)
         {
             if ((round.Game1 == null) || (round.Game2 == null))
             {
                 throw new WrongNumberOfPlayersErrorException();
+            }
+
+            var validStrategies = new string[] { "R", "P", "S" };
+
+            if ((!validStrategies.Contains(round.Game1.Strategy.ToUpper())) || (!validStrategies.Contains(round.Game2.Strategy.ToUpper())))
+            {
+                throw new NoSuchStrategyErrorException();
+            }
+
+            if (round.Game1.Strategy.ToUpper() == round.Game2.Strategy.ToUpper())
+            {
+                return round.Game1;
             }
 
             throw new NotImplementedException();
