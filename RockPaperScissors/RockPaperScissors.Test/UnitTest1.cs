ccea6e1d084db5ab9a4688aa6539bc8f703d82e6
abeppler <abeppler2000@gmail.com>
2019-06-12 18:08:14 -0300

    RpsGameWinner_ScissorShouldBeatPaper

--- RockPaperScissors/RockPaperScissors.Test/UnitTest1.cs
+++ RockPaperScissors/RockPaperScissors.Test/UnitTest1.cs
@@ -125,41 +125,71 @@ namespace RockPaperScissors.Test
             winnerRound2.PlayerName.Should().Be("Player2");
         }
 
+        [Fact]
+        public void RpsGameWinner_ScissorShouldBeatPaper()
+        {
+            //Arrange
+            var round1 = new Round
+            {
+                Game1 = new Game()
+                {
+                    PlayerName = "Player1",
+                    Strategy = "S"
+                },
+                Game2 = new Game()
+                {
+                    PlayerName = "Player2",
+                    Strategy = "P"
+                }
+            };
+
+            var round2 = new Round
+            {
+                Game1 = new Game()
+                {
+                    PlayerName = "Player1",
+                    Strategy = "P"
+                },
+                Game2 = new Game()
+                {
+                    PlayerName = "Player2",
+                    Strategy = "S"
+                }
+            };
+
+            //Act
+            var winnerRound1 = RpsGameWinner(round1);
+            var winnerRound2 = RpsGameWinner(round2);
+
+            //Assert
+            winnerRound1.PlayerName.Should().Be("Player1");
+            winnerRound2.PlayerName.Should().Be("Player2");
+        }
 
         private Game RpsGameWinner(Round round)
         {
             if ((round.Game1 == null) || (round.Game2 == null))
-            {
                 throw new WrongNumberOfPlayersErrorException();
-            }
 
             var validStrategies = new string[] { "R", "P", "S" };
 
             if ((!validStrategies.Contains(round.Game1.Strategy.ToUpper())) || (!validStrategies.Contains(round.Game2.Strategy.ToUpper())))
-            {
                 throw new NoSuchStrategyErrorException();
-            }
 
             if (round.Game1.Strategy.ToUpper() == round.Game2.Strategy.ToUpper())
-            {
                 return round.Game1;
-            }
 
-            if (round.Game1.Strategy == "R")
-            {
-                if (round.Game2.Strategy == "S")
-                {
-                    return round.Game1;
-                }
-            }
+            if ((round.Game1.Strategy == "R") && (round.Game2.Strategy == "S"))
+                return round.Game1;
 
-            if (round.Game2.Strategy == "R")
-            {
-                if (round.Game1.Strategy == "S")
-                {
-                    return round.Game2;
-                }
-            }
+            if ((round.Game2.Strategy == "R") && (round.Game1.Strategy == "S"))
+                return round.Game2;
+
+            if ((round.Game1.Strategy == "S") && (round.Game2.Strategy == "P"))
+                return round.Game1;
+
+            if ((round.Game2.Strategy == "S") && (round.Game1.Strategy == "P"))
+                return round.Game2;
 
             throw new NotImplementedException();
         }
